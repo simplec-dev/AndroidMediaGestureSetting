8a826cb3ca8712b69b90fbba8d42e303228e0d02
dnelson_simplec <dnelson@simplec.com>
2014-03-14 09:52:34 -0400

    .

--- src/android/com/simplec/phonegap/plugins/videosettings/AndroidMediaGestureSetting.java
+++ src/android/com/simplec/phonegap/plugins/videosettings/AndroidMediaGestureSetting.java
@@ -6,9 +6,15 @@ import org.apache.cordova.CordovaPlugin;
 
 public class AndroidMediaGestureSetting extends CordovaPlugin {
 
+    private static final String LOG_TAG = "AndroidMediaGestureSetting";
+
+
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: FALSE");
+
     }
 }
