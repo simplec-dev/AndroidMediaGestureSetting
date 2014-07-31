f0e2b88ee40ac6b69203221cccaec7be4027bbd9
Marc <marc@blitzr.com>
2014-07-31 12:08:00 +0200

    FIX Android version needed was 4.2

--- src/android/com/simplec/phonegap/plugins/videosettings/AndroidMediaGestureSetting.java
+++ src/android/com/simplec/phonegap/plugins/videosettings/AndroidMediaGestureSetting.java
@@ -2,21 +2,23 @@ package com.simplec.phonegap.plugins.videosettings;
 
 import org.apache.cordova.CordovaInterface;
 import android.util.Log;
+import android.os.Build;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaPlugin;
 
 public class AndroidMediaGestureSetting extends CordovaPlugin {
-
+    
     private static final String LOG_TAG = "AndroidMediaGestureSetting";
-
-
+    
+    
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
-	        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
-	        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: FALSE");
-        }
+	
+	if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+	    {
+		webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+		Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: FALSE");
+	    }
     }
 }
