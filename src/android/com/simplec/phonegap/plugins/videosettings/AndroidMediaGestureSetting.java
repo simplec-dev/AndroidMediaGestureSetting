de1ca3a478a350785c220eed6cdf9ee4f31b644b
doug.nelson <dnelson@simplec.com>
2015-09-01 19:40:15 -0400

    .

--- src/android/com/simplec/phonegap/plugins/videosettings/AndroidMediaGestureSetting.java
+++ src/android/com/simplec/phonegap/plugins/videosettings/AndroidMediaGestureSetting.java
@@ -1,5 +1,8 @@
 package com.simplec.phonegap.plugins.videosettings;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
@@ -7,7 +10,8 @@ import org.apache.cordova.CordovaWebView;
 import android.os.Build;
 import android.util.Log;
 import android.view.View;
-import android.webkit.WebSettings;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class AndroidMediaGestureSetting extends CordovaPlugin {
 
@@ -18,12 +22,42 @@ public class AndroidMediaGestureSetting extends CordovaPlugin {
         super.initialize(cordova, webView);
         
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-	        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
-	        
-	        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: FALSE");
+			try {
+				WebView view = getWebViewFromPlugin();
+	        	view.getSettings().setMediaPlaybackRequiresUserGesture(false);
+		        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: FALSE");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: ERROR");
+				Log.e(LOG_TAG, "error getting settings or view: "+e.getMessage());
+			}        	
         } else {
 	        Log.d(LOG_TAG, "setMediaPlaybackRequiresUserGesture: COULD NOT RUN");
         }
         
+    }
+    
+    public WebView getWebViewFromPlugin() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	WebView thisView = null;
+		boolean CORDOVA_4 = Integer.valueOf(CordovaWebView.CORDOVA_VERSION.split("\\.")[0]) >= 4;
+		Log.e(LOG_TAG, "CORDOVA_4: "+CORDOVA_4);
+		if (CORDOVA_4) {
+			if (webView.getClass().isAssignableFrom(WebView.class)) {
+				Log.e(LOG_TAG, "it is assignable");
+				thisView = ((WebView) webView);
+			} else {
+				Log.e(LOG_TAG, "using reflection to get method getView");
+				Method m = webView.getClass().getDeclaredMethod("getView", null);
+
+				Log.e(LOG_TAG, "got method: "+m);
+				thisView = (WebView) m.invoke(webView);//  webView.getView());
+				Log.e(LOG_TAG, "invoked method");
+			}
+		} else {
+			thisView = ((WebView) webView);
+		}
+		
+		return thisView;
     }
 }
